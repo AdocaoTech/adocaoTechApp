6508142d8dc54ceee137819cbee49a9d8aa3761e
FranciscoTei <francisco2mix@gmail.com>
2024-10-28 13:47:40 -0300

    :building_construction: refactor: Centraliado todas as rotas em routes.js

--- backend/src/routes/routes.js
+++ backend/src/routes/routes.js
@@ -1,9 +1,12 @@
 import { Router } from "express";
-
-import { petsRoutes } from "./petsRoutes";
+import { userRoutes } from "./userRoutes.js";
+import { petsRoutes } from "./petsRoutes.js";
+//import { adoptionRoutes } from "./adoptionRoutes.js";
 
 const router = Router();
 
+userRoutes(router);
 petsRoutes(router);
+//adoptionRoutes(router);
 
 export { router };
