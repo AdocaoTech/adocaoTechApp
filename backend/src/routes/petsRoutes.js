23bc7db956faa7fa8f95b4a837ddb97813ab7ede
FranciscoTei <francisco2mix@gmail.com>
2024-10-28 13:45:58 -0300

    :construction: feat: Adicionado o endpoint de getAllPets em petsRoutes.

--- backend/src/routes/petsRoutes.js
+++ backend/src/routes/petsRoutes.js
@@ -1,9 +1,10 @@
-import { PetController } from "../controllers/petController"
+import { PetController } from "../controllers/petController.js"
 
 const petController = new PetController();
 
 export function petsRoutes(router) {
     
+    router.get('/pets/', petController.getAllPets);
     router.get('/pet/:id', petController.getById);
     router.post('/pet/:id', petController.insertPet);
     // router.get('/pet/:id', petController.getById);
