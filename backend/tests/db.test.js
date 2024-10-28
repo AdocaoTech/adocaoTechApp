98589fd98820ccab13cdc4032ed0f9665ae1db9e
FranciscoTei <francisco2mix@gmail.com>
2024-10-28 13:49:38 -0300

    :mute: test: Skipados testes sem lógica nos endpoints.

--- backend/tests/db.test.js
+++ backend/tests/db.test.js
@@ -1,26 +1,7 @@
-const { PrismaClient } = require("@prisma/client");
-const fs = require("fs");
-const prisma = new PrismaClient();
-
-
-
-beforeAll(async () => {
-  await prisma.$connect();
-  await prisma.$executeRaw`PRAGMA foreign_keys = ON;`;
-});
-
-afterAll(async () => {
-  await prisma.$disconnect();
-  if (fs.existsSync("./test.db")) {
-    fs.unlinkSync("./test.db");
-  }
-});
+const { PrismockClient } = require("prismock");
+const prisma = new PrismockClient();
 
 describe("Pet Model", () => {
-  afterAll(async () => {
-    await prisma.$disconnect();
-  });
-
   it("teste cadastra novo pet", async () => {
     const newPet = await prisma.pet.create({
       data: {
