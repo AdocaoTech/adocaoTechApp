73aa50e43f1980a26e738a67b129f71853829796
FranciscoTei <francisco2mix@gmail.com>
2024-10-30 20:45:10 -0300

    :package: chore: update dependencies in package-lock.json

--- backend/src/controllers/petController.js
+++ backend/src/controllers/petController.js
@@ -1,13 +1,67 @@
+import prismaClient from "../database/prismaClient.js";
+
+
+export async function create(req, res) {
+    /**
+        #swagger.tags = ['Pets']
+        #swagger.description = 'Endpoint to create a new pet.'
+        #swagger.summary = 'Create a new pet.'
+    */
+
+    try {
+        const { name, specie, description, size, personality, dob } = req.body;
+
+        const pet = await prismaClient.pet.create({
+            data: {
+                name,
+                specie,
+                description,
+                size,
+                personality,
+                dob: new Date(),
+            },
+        });
+        // #swagger.responses[201] = { description: 'User registered successfully.' }
+        return res.status(201).json({ pet });
+    } catch (error) {
+        // #swagger.responses[500] = { description: 'Server error.' }
+        return res.status(500).json({ error: error.message });
+    }
+}
+
+export async function getAll(req, res) {
+  const { limit = 10, offset = 0 } = req.query;
+  /**
+    #swagger.tags = ['Pets']
+    #swagger.description = 'Endpoint to get all pets.'
+    #swagger.summary = 'gGet all pets.'
+  */
+  try {
+    const pets = await prismaClient.pet.findMany({
+      skip: parseInt(offset),
+      take: parseInt(limit),
+      select: {
+        id: true,
+        name: true,
+        dob: true,
+        description: true,
+        status: true,
+      },
+    });
+    // #swagger.responses[200] = { description: 'Successful request.' }
+    return res.status(200).json({ pets });
+  } catch (error) {
+    console.error({ error });
+    // #swagger.responses[500] = { description: 'Server error.' }
+    return res.status(500).json({ error: error.message });
+  }
+}
+
 export class PetController {
 
-    async getAllPets(req, res){}
+  async getOneById(req, res) {}
 
-    async getById(req, res){}
+  async update(req, res) {}
 
-    async insertPet(req, res){}
-
-    async updatePet(req, res){}
-
-    async deletePet(req, res){}
-
+  async delete(req, res) {}
 }
