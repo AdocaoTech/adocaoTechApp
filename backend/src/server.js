4572d1e87f7b676d0a8e377ab0acc67a525ecd37
FranciscoTei <francisco2mix@gmail.com>
2024-10-30 20:48:20 -0300

    :rocket: feat: configure server

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,16 +1,19 @@
 import express from "express";
 import cors from "cors";
-import {router} from "./routes/routes.js";
+import routes from "./routes/routes.js";
+import swaggerUi from "swagger-ui-express";
 
+import swaggerFile from "./swagger-output.json" assert { type: "json" };
 
 
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use("/", router);
+app.use("/", routes);
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 const PORT = 3000;
 
 app.listen(PORT, () => {
-  console.log(`Running server on port ${PORT}`);
+  console.log(`API documentation: http://localhost:${PORT}/docs`);
 });
