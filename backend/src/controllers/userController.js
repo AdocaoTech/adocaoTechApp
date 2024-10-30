e792c094d55a05ae12d8e183f4cf4e0c0300dbdf
FranciscoTei <francisco2mix@gmail.com>
2024-10-30 20:45:52 -0300

    :construction: feat: add basic Swagger structure to user endpoints

--- backend/src/controllers/userController.js
+++ backend/src/controllers/userController.js
@@ -1,13 +1,42 @@
-export class UserController {
-
-    async getById(req, res){}
-
-    async insertUser(req, res){}
-
-    async updateUser(req, res){}
-
-    async deleteUser(req, res){}
-
-    async validateLogin(req, res){}
-
+export async function getAll(req, res) {
+/**
+    #swagger.tags = ['User']
+    #swagger.description = 'Endpoint to get all users.'
+    #swagger.summary = 'Get all users.'
+*/
+}
+export async function getById(req, res) {
+/**
+    #swagger.tags = ['User']
+    #swagger.description = 'Endpoint to get user by ID.'
+    #swagger.summary = 'Get user by ID.'
+*/
+}
+export async function create(req, res) {
+  /**
+    #swagger.tags = ['User']
+    #swagger.description = 'Endpoint to create a new user.'
+    #swagger.summary = 'Create a new user.'
+*/
+}
+export async function updateUser(req, res) {
+/**
+    #swagger.tags = ['User']
+    #swagger.description = 'Endpoint to update an existing user.'
+    #swagger.summary = 'Update an existing user.'
+*/
+}
+export async function deleteUser(req, res) {
+/**
+    #swagger.tags = ['User']
+    #swagger.description = 'Endpoint to delete a user.'
+    #swagger.summary = 'Delete a user.'
+*/
+}
+export async function validateLogin(req, res) {
+/**
+    #swagger.tags = ['User']
+    #swagger.description = 'Endpoint to validate user login.'
+    #swagger.summary = 'Validate user login.'
+*/
 }
